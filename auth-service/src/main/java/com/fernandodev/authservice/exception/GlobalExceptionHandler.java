9ff92f9ea450a653297cfde2aa12e85d2988a92f
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2025-08-25 11:58:51 -0300

    test

--- auth-service/src/main/java/com/fernandodev/authservice/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/com/fernandodev/authservice/exception/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    // hello
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
