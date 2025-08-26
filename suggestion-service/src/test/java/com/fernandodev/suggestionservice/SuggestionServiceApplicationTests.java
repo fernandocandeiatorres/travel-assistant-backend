43a93629f416d61470b435704e1455f9890365cb
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2025-08-26 12:21:47 -0300

    fix

--- suggestion-service/src/test/java/com/fernandodev/suggestionservice/SuggestionServiceApplicationTests.java
+++ suggestion-service/src/test/java/com/fernandodev/suggestionservice/SuggestionServiceApplicationTests.java
@@ -2,12 +2,25 @@ package com.fernandodev.suggestionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.fernandodev.suggestionservice.repository.SuggestionRepository;
+import com.fernandodev.suggestionservice.repository.ItineraryRepository;
 
 @SpringBootTest
 class SuggestionServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@MockBean
+	private RabbitTemplate rabbitTemplate;
+
+	@MockBean
+	private SuggestionRepository suggestionRepository;
+	
+	@MockBean
+	private ItineraryRepository itineraryRepository;
+
+	@Test
+	void contextLoads() {
+	}
 
 }
